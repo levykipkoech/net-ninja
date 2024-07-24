29acb7a93ac19718f41cf88b4ef7e77529831f3a
levy kipkoech <kipkoechlevy096@gmail.com>
2024-07-24 16:10:39 +0300

    added reques routes

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
 
-@Controller('ninjas')
-export class NinjasController {}
+@Controller("ninjas")
+export class NinjasController {
+  //get ninjas
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  //get ninjas"id"
+  @Get(":id")
+  getOneNinja() {
+    return {};
+  }
+  //posti ninja
+  @Post()
+  createNinja() {
+    return {};
+  }
+  //updating the ninja "id"
+  @Put(":id")
+  updateNinja() {
+    return {};
+  }
+  @Delete(":id")
+  deleteNinj() {
+    return {};
+  }
+}
